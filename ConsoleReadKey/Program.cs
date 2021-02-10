c2a4ccf9cfb04a52ddb48fdd31b8415d6f66ff86
Myster-Sneppi <60942937+Alex-ViGiL@users.noreply.github.com>
2021-02-10 17:43:11 +0200

    ConsoleReadKey

---
+++ ConsoleReadKey/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleReadKey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ConsoleKeyInfo key = Console.ReadKey();
+            char ch = key.KeyChar;
+
+
+
+            Console.WriteLine("Symbol = "+ch);
+            
+
+            Console.ReadKey();
+
+        }
+    }
+}
